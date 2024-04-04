6aaa7643f1d53f857d612d83507f53953547b060
spustoff <vyacheslavpustov05@gmail.com>
2024-04-04 13:16:11 +0300

    1.1 update

--- App425/Project/LoadingView.swift
+++ App425/Project/LoadingView.swift
@@ -13,13 +13,13 @@ struct LoadingView: View {
         
         ZStack {
             
-            Color("primary")
+            Color(red: 34/255, green: 83/255, blue: 31/255)
                 .ignoresSafeArea()
             
             Image("logo_big")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 200, height: 200)
+                .frame(width: 135, height: 135)
             
             VStack {
                 
